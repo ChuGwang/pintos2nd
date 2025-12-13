b4efd444cf60012913d025d954f40ee505f6d4fc
ChuGwang <3387asqw@naver.com>
2025-12-13 21:55:22 +0900

    Update palloc.c

--- threads/palloc.c
+++ threads/palloc.c
@@ -229,8 +229,16 @@ init_pool(struct pool *p, void *base, size_t page_cnt, const char *name)
         PANIC("Not enough memory in %s for bitmap.", name);
     page_cnt -= bm_pages;
 
-    printf("%zu pages available in %s.\n", page_cnt, name);
 
+
+
+   // 원래 있던 코드인데 printf 때문에 오류 나는 걸 수도 있다고 해서 일단 주석 처리
+    //printf("%zu pages available in %s.\n", page_cnt, name);
+
+
+
+
+   
     /* Initialize the pool. */
     lock_init(&p->lock);
     p->used_map = bitmap_create_in_buf(page_cnt, base, bm_pages * PGSIZE);
